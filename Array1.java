492b7199beaca5f08b154c4ad64a414fd57ecf4e
Agung Sukariman <agungsukariman@gmail.com>
2018-02-24 11:42:29 +0700

    Array Class Baru

---
+++ Array1.java
@@ -0,0 +1,7 @@
+class Array1{
+    public static void main (String [] args){
+    int [] month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; 
+    System.out.println("April has "+month[4]+ " days.");
+    
+}
+}
